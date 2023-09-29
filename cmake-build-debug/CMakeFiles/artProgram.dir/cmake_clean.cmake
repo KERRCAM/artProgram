14a02830f87b99bc23285956bdad65dd8c6e5bd2
KERRCAM <kerrcameron@hotmail.com>
2023-09-29 14:20:42 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/artProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/artProgram.dir/main.cpp.o"
+  "artProgram"
+  "artProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/artProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
