23d89b96e0c11711fe3a713fb7e43a0810c389a5
KERRCAM <kerrcameron@hotmail.com>
2023-10-26 16:40:33 +0100

    get string

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+using namespace std;
+
+
+class coreMethods{
+public:
+    string getString(string prompt){
+        cout << prompt << endl;
+        string answer;
+        cin >> answer;
+        return answer;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl; 
-    std::cout << "Hello, World!" << std::endl;
+    coreMethods CM;
+    bool exit = false;
+    while (exit == false){
+        string answer = CM.getString("What would you like to do? (enter number of action): \n (1)-Sign in \n (2)-Create account");
+        if (answer == "1"){
+            exit = true;
+            cout << 0 << endl;
+        }
+        if (answer == "2"){
+            exit = true;
+            cout << 0 << endl;
+        }
+        else{
+            cout << "please enter a valid number" << endl;
+        }
+    }
+
+
+
     return 0;
 }
+
