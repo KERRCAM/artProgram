b713e93bb995ad286cc2f45ebbc99196694080d4
KERRCAM <kerrcameron@hotmail.com>
2023-11-01 20:04:06 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@ public:
     int createFolder(){
         return 0;
     }
+
+    int deleteFolder(){
+        return 0;
+    }
 };
 
 
@@ -42,14 +46,18 @@ int main() {
     folderSelect FS;
     bool exit = false;
     while (exit == false){
-        string answer = CM.getString("What would you like to do? (enter number of action): \n (1)-Open folder \n (2)-Create new folder");
+        string answer = CM.getString("What would you like to do? (enter number of action): \n (1)-Open folder \n (2)-Create new folder \n (3)-Delete folder");
         if (answer == "1"){
             exit = true;
-            cout << 0 << endl;
+            FS.openFolder();
         }
         if (answer == "2"){
             exit = true;
-            cout << 0 << endl;
+            FS.createFolder();
+        }
+        if (answer == "3"){
+            exit = true;
+            FS.deleteFolder();
         }
         else{
             cout << "please enter a valid number" << endl;
