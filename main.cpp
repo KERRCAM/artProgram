d1094e010d3f238b08fa1bc6900ca7e90d553e76
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-11-01 20:53:10 +0000

    menu loop

--- main.cpp
+++ main.cpp
@@ -113,18 +113,18 @@ int main() {
     folderSelect FS;
     bool exit = false;
     while (exit == false){
-        string answer = CM.getString("What would you like to do? (enter number of action): \n (1)-Open folder \n (2)-Create new folder \n (3)-Delete folder");
+        string answer = CM.getString("What would you like to do? (enter number of action): \n (1)-Open folder \n (2)-Create new folder \n (3)-Delete folder \n (4)-Exit");
         if (answer == "1"){
-            exit = true;
             FS.openFolder();
         }
         if (answer == "2"){
-            exit = true;
             FS.createFolder();
         }
         if (answer == "3"){
-            exit = true;
             FS.deleteFolder();
+        }
+        if (answer == "4"){
+            exit = true;
         }
         else{
             cout << "please enter a valid number" << endl;
