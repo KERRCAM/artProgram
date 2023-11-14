ed2144311dd8ae352a7be708b1dd0c00110bd6fe
KERRCAM <kerrcameron@hotmail.com>
2023-11-14 09:13:51 +0000

    paint methods layout

--- paintMethods.cpp
+++ paintMethods.cpp
@@ -2,10 +2,17 @@
 
 class paintMethods{
 private:
-
+    void draw(){
+        //needs to listen for all commands and mouse movement etc and call corresponding methods
+    }
 
 
 public:
+    void openFile(string file){
 
+    }
 
+    void saveFile(string file){
+
+    }
 };
--- paintMethods.h
+++ paintMethods.h
@@ -1,7 +1,12 @@
 #ifndef ARTPROGRAM_PAINTMETHODS_H
 #define ARTPROGRAM_PAINTMETHODS_H
+#include <iostream>
+using namespace std;
 
+void openFile(string file);
 
+void saveFile(string file);
 
+void draw();
 
 #endif //ARTPROGRAM_PAINTMETHODS_H
